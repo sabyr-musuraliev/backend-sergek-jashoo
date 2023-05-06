f815a91132b33387304fd0e4a81478d805c8dee7
Sabyr <sabyr.shogun@gmail.com>
2023-05-07 01:12:28 +0600

    auth changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors(allowedOrigins));
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
-app.use("/auth", auth);
+// app.use("/auth", auth);
 app.use("/patients", patients);
 
 const PORT = config.get("port") || 5000;
